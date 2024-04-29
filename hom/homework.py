9fb1a277157a80e2f25e6e32d412dabae684658c
caleb <cmcmillan050@gmail.com>
2024-04-29 17:21:19 -0400

    Homework

---
+++ hom/homework.py
@@ -0,0 +1,34 @@
+#question 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+#question 2
+mood=input("how are you feeling")
+print(f"I am feeling {mood}")
+if mood == "happy":
+    print("Thats great to hear!")
+if mood == "sad":
+    print("I hope your day gets better!")
+
+# question 3
+pi_value = 3.14
+user_age = 25 # i kept it the same because there age can change.
+USER_LOCATION = "New York"
+MAX_LIMIT = 1000
+
+#question 4
+Honey = 4
+Bread = 5
+Sugar = 6
+Total_Cost = (Honey + Bread + Sugar)
+print(f"${Total_Cost}")
+
+# question 5
+Bank_Account=10000
+int=7
+years=1
+future_value = Bank_Account * ((1 + (0.01 * int)) ** years)
+print(round(future_value, 2))
